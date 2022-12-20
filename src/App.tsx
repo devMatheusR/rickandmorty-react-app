30dabe2bf03746c3521353da3726b21278db4cc9
devMatheusR <devmatheusr@gmail.com>
2022-12-20 10:00:05 -0300

    -a

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,55 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import api from "./config/api";
-import { AxiosResponse } from "axios";
+import api from "./services/api";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
-  const [platform, setPlatform] = useState<string>("Steam");
-  const [username, setUsername] = useState<string>("");
-  const [results, setResults] = useState<any[]>([]);
+  const [characteres, setCharacteres] = useState<any[]>([]);
 
-  const getPlayerData = async (platform: string, playerUsername: string) => {
+  const getAllCharacteres = async () => {
     console.log("chamando aq");
     await api
-      .get(``, {
-        params: { platform: platform, query: playerUsername },
-      })
+      .get(`/character`)
       .then((response) => {
-        console.log(response.data);
-      });
+        console.log(response.data.results);
+        setCharacteres(response.data.results);
+      })
+      .catch(console.error);
   };
-  //https://public-api.tracker.gg/v2/csgo/standard/search?platform=steam&query=padawann__
+
+  
   const handleSubmit = async (e: any) => {
-    getPlayerData(platform, username);
+    e.preventDefault();
+    getAllCharacteres();
   };
 
   return (
     <div className="App">
-      <div className="card">
-        <button type="submit" onClick={handleSubmit}>
-          <p>Get player data</p>
-        </button>
+      <button type="submit" onClick={handleSubmit}>
+        <p>Get characteres</p>
+      </button>
+      <div className="char-container">
+        <h1> Rick and Morty</h1>
+
+        <Container>
+          <Row >
+            {characteres.map((char) => (
+              <Col>
+                <img src={char.image} alt="Logo" />
+                <b>Nome:</b>
+                {char.name}
+                <br />
+                <b>Status:</b>
+                {char.status}
+                <br />
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </div>
-      <form>
-        <p>
-          <label htmlFor="inputName">Nome: </label>
-          <input
-            className="inputName"
-            datatype="string"
-            defaultValue={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </p>
-        <p>
-          <label htmlFor="plataforma">Plataforma: </label>
-          <input
-            className="inputPlataforma"
-            datatype="string"
-            defaultValue={platform}
-            onChange={(e) => setPlatform(e.target.value)}
-          />
-        </p>
-      </form>
-      <div>{results}</div>
-      
     </div>
   );
 }
